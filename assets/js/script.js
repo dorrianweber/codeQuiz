b3130403a7d732471a21bd6769bdbf2cda3dc87b
Dorrian Weber <dorrianweber@gmail.com>
2021-01-21 18:10:09 -0800

    changed up timer display, added a current score display, reorganized html elements, got it so clicking an answer button only creates a next button one time, and so clicking the next button loads the next question with answer buttons, also made it so when the timer runs out the game ends & user is prompted to enter their initials for score saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,74 @@
 var timer = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
-var secondsLeft = 10;
 var header = document.querySelector("#header");
 var content = document.querySelector("#content");
-var qIndex = 0;
+var pageBody =  document.querySelector("#pageBody");
 var buttonSection = document.querySelector(".center");
+
+var secondsLeft = 10;
+var qIndex = 0;
+var incorrectAnswers = 0;
+var correctAnswers = 0;
+var currentScore = 0;
+
+var userScores = [];
 
 // Array of objects, each object has an array within it
 var questions = [
   {
     q: "Question 1",
-    a: ["answer1", "answer2", "answer3"],
-    correct: "answer3",
+    a: ["q1_a1", "q1_a2", "q1_a3"],
+    correct: "a3",
   },
 
   {
     q: "Question 2",
-    a: ["answer1", "answer2", "answer3"],
-    correct: "answer1",
+    a: ["q2_a1", "q2_a2", "q2_a3"],
+    correct: "a1",
+  },
+
+  {
+    q: "Question 3",
+    a: ["q3_a1", "q3_a2", "q3_a3"],
+    correct: "a2",
+  },
+
+  {
+    q: "Question 4",
+    a: ["q4_a1", "q4_a2", "q4_a3"],
+    correct: "a1",
   },
 ]
 
-
+// Controls the timer in the top center of the page
 function setTime() {
   displayQuestion();
-  // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timer.textContent = secondsLeft;
+    timer.textContent = "Time left: " + secondsLeft + " seconds";
 
     if(secondsLeft === 0) {
-      // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
+      gameOver();
     }
 }, 1000)};
 
+function storeScore () {
+  localStorage.setItem("")
+};
+
+// Shows the "game over" message when user runs out of time
+function gameOver() {
+  var userInitials = prompt("Thanks for playing! Enter your initials to save your score.");
+  storeScore();
+}
+
+// Shows user each question after they hit "start" & after clicking "next" after answering each question
 function displayQuestion() {
   header.textContent = questions[qIndex].q;
   
+  buttonSection.innerHTML = "";
+
   for (var i = 0; i < questions[qIndex].a.length; i++) {
     var answerButton = document.createElement("button");
     content.appendChild(answerButton);
@@ -54,17 +83,33 @@ function displayQuestion() {
 function compareAnswers(event) {
   if (event.target.matches("button")){
     console.log(event.target);
-
+    buttonSection.innerHTML = "";
     var nextButton = document.createElement("button");
-
     buttonSection.appendChild(nextButton);
     nextButton.textContent = "Next";
-}};
+  }
 
-// function sendMessage() {
-    
-// };
+  nextButton.addEventListener("click", function() {
+    qIndex ++;
+    header.innerHTML = "";
+    content.innerHTML = "";
+    displayQuestion();
+  });
+};
 
+// if (press incorrect answer) {
+//  timer - 3 seconds;
+//  incorrectAnswers ++;
+//  incorrectAnswers --> local storage;
+// }
+
+// if (press correct answer) {
+//  timer + 3 seconds;
+//  correctAnswers ++;
+//  correctAnswers --> local storage;
+// }
+
+// Clears intro text content & starts the timer when user hits "start" button
 startBtn.addEventListener("click", function() {
   content.innerHTML = "";
   buttonSection.innerHTML = "";
