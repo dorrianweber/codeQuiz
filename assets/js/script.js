084ee5d84e4223c9f8d9f6f950f052fdba5d6eb4
Dorrian Weber <dorrianweber@gmail.com>
2021-01-23 16:09:59 -0800

    Trying to figure out how to compare answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,40 @@ var header = document.querySelector("#header");
 var content = document.querySelector("#content");
 var pageBody =  document.querySelector("#pageBody");
 var buttonSection = document.querySelector(".center");
+var scoreDisplay = document.querySelector("#score-display")
 
 var secondsLeft = 10;
 var qIndex = 0;
 var incorrectAnswers = 0;
 var correctAnswers = 0;
-var currentScore = 0;
+var currentScore = 0
 
-var userScores = [];
+scoreDisplay.textContent = "Current score: " + currentScore;
 
 // Array of objects, each object has an array within it
 var questions = [
   {
     q: "Question 1",
     a: ["q1_a1", "q1_a2", "q1_a3"],
-    correct: "a3",
+    correct: "q1_a3",
   },
 
   {
     q: "Question 2",
     a: ["q2_a1", "q2_a2", "q2_a3"],
-    correct: "a1",
+    correct: "q2_a1",
   },
 
   {
     q: "Question 3",
     a: ["q3_a1", "q3_a2", "q3_a3"],
-    correct: "a2",
+    correct: "q3_a2",
   },
 
   {
     q: "Question 4",
     a: ["q4_a1", "q4_a2", "q4_a3"],
-    correct: "a1",
+    correct: "q4_a1",
   },
 ]
 
@@ -49,18 +50,21 @@ function setTime() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
+      content.innerHTML = "";
+      header.textContent = "";
       gameOver();
     }
 }, 1000)};
 
-function storeScore () {
-  localStorage.setItem("")
+// Puts user's initials & score in local storage
+function storeScore (userInitials) {
+  localStorage.setItem(userInitials, currentScore);
 };
 
-// Shows the "game over" message when user runs out of time
+// Shows the "game over" message when user runs out of time & prompts them to enter their initials
 function gameOver() {
   var userInitials = prompt("Thanks for playing! Enter your initials to save your score.");
-  storeScore();
+  storeScore(userInitials);
 }
 
 // Shows user each question after they hit "start" & after clicking "next" after answering each question
@@ -77,12 +81,19 @@ function displayQuestion() {
   
   content.addEventListener("click", function(event) {
     compareAnswers(event);
+    // console.log(event.target.textContent);
+    // console.log(questions[qIndex].correct);
+    if (event.target.textContent === questions[qIndex].correct) {
+      currentScore ++;
+    }
   })
 };
 
+// Creates a "next" button when user clicks on one of the answer options that moves onto the next quesiton
 function compareAnswers(event) {
-  if (event.target.matches("button")){
-    console.log(event.target);
+console.log(qIndex);
+
+  if (event.target.matches("button")) {
     buttonSection.innerHTML = "";
     var nextButton = document.createElement("button");
     buttonSection.appendChild(nextButton);
